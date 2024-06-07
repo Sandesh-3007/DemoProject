bb078f5be544db30f863c7b08a6f70d0af9ed02a
Harish <Harish@TP-50181>
2024-06-07 09:15:07 +0530

    committed Harish class

---
+++ SeleniumMaven8/src/test/java/harish/harishClass.java
@@ -0,0 +1,12 @@
+package harish;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class harishClass {
+	@Test
+	public void demo() {
+		Reporter.log("Harish classs added", true);
+	}
+
+}
