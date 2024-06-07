c428373e7e6f4b811f0ff2df943ea6fff332792e
SandeshShetty <SandeshShetty@Sandesh>
2024-06-07 09:11:03 +0530

    Sandesh changed

---
+++ SeleniumMaven8/src/test/java/Sandesh_Demo/sandeshDemo.java
@@ -0,0 +1,14 @@
+package Sandesh_Demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class sandeshDemo 
+{
+	@Test
+	public void demo()
+	{
+		Reporter.log("Sandesh is a collaborator",true);
+	}
+
+}
